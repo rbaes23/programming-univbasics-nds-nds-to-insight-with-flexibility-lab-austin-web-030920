db23795215bbde6919a9ceededab514e78ec8f2a
Rochelle Bondoc <rochelleannebaes@gmail.com>
2020-02-26 20:22:55 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -58,7 +58,7 @@ def movies_with_director_key(name, movies_collection)
     result << movie_with_director_name(name, movie_data)
     i += 1
   end
-  result
+  pp result
 end
 
   # GOAL: Given an Array of Hashes where each Hash represents a movie,
